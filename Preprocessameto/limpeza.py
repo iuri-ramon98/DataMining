41f0a37802beb4e035dae4b6da28ec9e61e47a31
Iuri Ramon <iuriramon@yahoo.com.br>
2021-04-07 09:06:30 -0300

    Histograma

--- Preprocessameto/limpeza.py
+++ Preprocessameto/limpeza.py
@@ -5,10 +5,13 @@ import numpy as np
 def main():
     # Faz a leitura do arquivo
     input_file = 'bridges.data.version2'
+    output_file = 'bridges.data.version2clean'
     df = pd.read_csv(input_file, # Nome do arquivo com dados
                      names =['IDENTIF','RIVER','LOCATION','ERECTED','PURPOSE','LENGTH', 'LANES', 'CLEAR-G', 'T-OR-D', 'MATERIAL', 'SPAN', 'REL-L','TYPE'], # Nome das colunas 
-                     usecols = ['LOCATION','LANES'], # Define as colunas que serão  utilizadas
+                     usecols = ['LOCATION'], # LOCATION Define as colunas que serão  utilizadas
                      na_values='?') # Define que ? será considerado valores ausentes
+    
+    df_original = df.copy()
     
     # Imprime as 15 primeiras linhas do arquivo
     print("PRIMEIRAS 15 LINHAS\n")
@@ -51,8 +54,8 @@ def main():
 
     elif method == 'mean':
         # Substituindo valores ausentes pela média
-        mean = df['Density'].mean()
-        df['Density'].fillna(mean, inplace=True)    
+        mean = df['LOCATION'].mean()
+        df['LOCATION'].fillna(mean, inplace=True)    
       
     elif method == 'mode':
         # Substituindo valores ausentes pela moda
@@ -61,11 +64,48 @@ def main():
         df['LOCATION'].fillna(mode, inplace=True)    
     
     
+    columns_missing_value = df.columns[df.isnull().any()]
+    print(columns_missing_value)
+    method = 'mode' # number or median or mean or mode
+    
+    for c in columns_missing_value:
+        UptateMissingvalue(df, c)
+    
     print('Total valores ausentes: ' + str(df['LOCATION'].isnull().sum()))
     print(df.describe())
     print("\n")
-
+    print(df.head(15))
+    print(df_original.head(15))
     print("\n")
+    
+    # Salva arquivo com o tratamento para dados faltantes
+    df.to_csv(output_file, header=False, index=False)  
+    
+
+def UptateMissingvalue(df, column, method="mode", number=0):
+    if method == 'number':
+        # Substituindo valores ausentes por um número
+        df[column].fillna(number, inplace=True)
+    elif method == 'median':
+        # Substituindo valores ausentes pela mediana 
+        median = df['LOCATION'].median()
+        df[column].fillna(median, inplace=True)
+    elif method == 'mean':
+        # Substituindo valores ausentes pela média
+        mean = df[column].mean()
+        df[column].fillna(mean, inplace=True)
+    elif method == 'mode':
+        # Substituindo valores ausentes pela moda
+        mode = df[column].mode()[0]
+        df[column].fillna(mode, inplace=True)
+    
+    #print('Total valores ausentes: ' + str(df['LOCATION'].isnull().sum()))
+    #print(df.describe())
+    #print("\n")
+
+    #print("\n")
+    
+    
     
 
 if __name__ == "__main__":
